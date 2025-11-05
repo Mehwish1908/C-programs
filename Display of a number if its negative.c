af245cc3e981db329f436b588166de630de2046d
Mehwish1908 <mehwish.f25@gcu.edu.in>
2025-11-05 18:21:40 +0530

    Add files via upload

---
+++ Display of a number if its negative.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int n;
+    printf("Enter an integer:\n");
+    scanf("%d",&n);
+    if (n<0)
+    {
+       printf("You entered %d \n", n);
+    }
+        printf("The if statement is easy.");
+    return 0;
+}
+
