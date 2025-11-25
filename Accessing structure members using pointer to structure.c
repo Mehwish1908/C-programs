2639e56b94b1eccf7f7ba34662b0bf0c2449d59a
Mehwish1908 <mehwish.f25@gcu.edu.in>
2025-11-25 10:34:43 +0530

    Add files via upload

---
+++ Accessing structure members using pointer to structure.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Student
+{
+    int id;
+    char name[20];
+};
+int main()
+{
+    struct Student s={101,"Mehwish"};
+    struct Student*ptr=&s;
+    printf("ID:%d\n",ptr->id);
+    printf("Name:%s\n",ptr->name);
+    return 0;
+}
