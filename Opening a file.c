5cde7f229803b1e76e29c966bce9d6299188468d
Mehwish1908 <mehwish.f25@gcu.edu.in>
2026-02-04 18:11:16 +0530

    Add files via upload

---
+++ Opening a file.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    char path[200]="start C:\\Users\\mehwi\\OneDrive\\Desktop\\Mehwish\\userinfoo.txt";
+    printf("Opening .....\n");
+    system(path);
+    return 0;
+}
