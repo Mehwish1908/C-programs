18b0bce181210c659a1cd703d3a2396e88d88891
Mehwish1908 <mehwish.f25@gcu.edu.in>
2026-02-04 16:14:56 +0530

    Add files via upload

---
+++ Opening a file.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    char path[100];
+    printf("Enter the path of the file to open: ");
+    scanf("%s",path);
+    system(path);
+    return 0;
+}
